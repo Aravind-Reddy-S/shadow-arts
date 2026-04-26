178676c54a8ae5076319127ee9f530116702aa9c
Aravind Reddy <aravindreddy1916@gmail.com>
2026-04-26 16:45:31 +0530

    clean shadow arts project

---
+++ backend/shadowarts/venv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:\Users\akhil\Webyog_SQLyog_6.56_Enterprise\Webyog_SQLyog_6.56_Enterprise\Desktop\Shadow2\smooth-user-flow\backend\shadowarts\venv\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
